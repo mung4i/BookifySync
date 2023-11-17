02a3ded2d9ff19b87e42ade642fadaa19bbf23b7
Martin Mungai <mungaimrtn@gmail.com>
2023-11-17 22:00:07 +0300

    integrate GoogleSignIn

--- BookifySync/BookifySyncApp.swift
+++ BookifySync/BookifySyncApp.swift
@@ -7,14 +7,59 @@
 
 import SwiftUI
 
+import GoogleSignIn
+import GoogleSignInSwift
+
 @main
 struct BookifySyncApp: App {
     let persistenceController = PersistenceController.shared
+    
+//    @UIApplicationDelegateAdaptor(AppDelegate.self)
+//    private var appDelegate
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AuthView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .onAppear {
+                    GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+                        if error != nil || user == nil {
+                            // sign out state
+                        } else {
+                            // sign in state
+                        }
+                    }
+                }
+                .onOpenURL { url in
+                    GIDSignIn.sharedInstance.handle(url)
+                }
         }
     }
 }
+
+struct AuthView: View {
+    var body: some View {
+        VStack {
+            
+            GoogleSignInButton(action: handleSignIn)
+            
+        }
+    }
+    
+    private func handleSignIn() {
+        GIDSignIn.sharedInstance.signIn(
+            withPresenting: UIHostingController(rootView: self)
+        ) { result, error in
+            
+            guard let result else {
+                // Check error
+                
+                return
+            }
+            
+            
+        }
+    }
+}
+
+
