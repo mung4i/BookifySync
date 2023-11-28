cbf3644f65ba89efd9b0719c534999ae32bd7c9d
Martin Mungai <mungaimrtn@gmail.com>
2023-11-28 20:41:19 +0300

    update models

---
+++ BookifySync/Domain/Model/TabSelectionState.swift
@@ -0,0 +1,16 @@
+//
+//  TabSelectionState.swift
+//  BookifySync
+//
+//  Created by Martin Mungai on 28/11/2023.
+//
+
+import Foundation
+
+enum TabSelectionState: Int, CaseIterable {
+    case messages
+    case offers
+    case calendar
+    case bookings
+    case menu
+}
