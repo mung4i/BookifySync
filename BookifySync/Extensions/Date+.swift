4aefdf73216af2ea0a98d491d7a8c6c1e9eee0c1
Martin Mungai <mungaimrtn@gmail.com>
2023-11-28 21:24:35 +0300

    refactor date

--- BookifySync/Extensions/Date+.swift
+++ BookifySync/Extensions/Date+.swift
@@ -8,6 +8,16 @@
 import Foundation
 
 extension Date {
+    static func advanceDate(
+        component: Calendar.Component = .day,
+        value: Int = 1,
+        date: Date = Date()
+    ) -> Date {
+        Calendar.current.date(
+            byAdding: component,
+            value: value,
+            to: date)!
+    }
     
     func formatDateWithoutNewline(_ format: String = "") -> String {
         let dateFormatter = DateFormatter()
@@ -24,15 +34,26 @@ extension Date {
         let date: String = dateFormatter.string(from: self)
         return date.addNewLineAfterSpace()
     }
+        
+    func getDates(start: Date, endDate: Date) -> [Date] {
+        let calendar = Calendar.current
+        var dates: [Date] = []
+        var currentDate = start
+
+        while currentDate <= endDate {
+            dates.append(currentDate)
+            currentDate = Date.advanceDate(date: currentDate)
+        }
+
+        return dates
+    }
     
-    static func advanceDate(
-        component: Calendar.Component = .day,
-        value: Int = 1,
-        date: Date = Date()
-    ) -> Date {
-        Calendar.current.date(
-            byAdding: component,
-            value: value,
-            to: date)!
+    func toDay() -> Int {
+        toNumber(format: "d")
+    }
+    
+    func toNumber(format: String = "MM") -> Int {
+        formatDate(format)
+            .toInt()
     }
 }
