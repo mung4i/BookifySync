deeb2a3cd04b9695fb4b82eefab88259fe500f83
Martin Mungai <mungaimrtn@gmail.com>
2023-11-28 15:26:05 +0300

    create viewModifier for pill view

---
+++ BookifySync/UI/Components/PillView/PillView+.swift
@@ -0,0 +1,29 @@
+//
+//  PillView+.swift
+//  BookifySync
+//
+//  Created by Martin Mungai on 27/11/2023.
+//
+
+import SwiftUI
+
+extension View {
+    func pillView(
+        action: @escaping Action,
+        name: String,
+        width: CGFloat,
+        height: CGFloat = 45,
+        padding: CGFloat = 25,
+        isHidden: Bool = false
+    ) -> some View {
+        modifier(
+            PillViewModifier(
+                action: action,
+                name: name,
+                width: width,
+                height: height,
+                padding: padding,
+                isHidden: isHidden)
+        )
+    }
+}
